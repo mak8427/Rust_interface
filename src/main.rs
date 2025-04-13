3070733c38e10172c4f2d913a91936a84932d570
Davide <davide.mattioli.2001@gmail.com>
2025-04-13 22:20:00 +0200

    Stuff

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
-fn main() {
-    println!("Hello, world!");
+use iced::{Element, Settings, run};
+use iced::widget::{button, column, text, Column};
+
+#[derive(Default)]
+struct Counter {
+    value: i64,
+}
+
+#[derive(Debug, Clone, Copy)]
+enum Message {
+    Increment,
+    Decrement,
+}
+
+fn update(counter: &mut Counter, message: Message) {
+    match message {
+        Message::Increment => counter.value += 1,
+        Message::Decrement => counter.value -= 1,
+    }
+}
+
+fn view(counter: &Counter) -> Element<Message> {
+
+    column![
+        text("Shit!"),
+        button("+").on_press(Message::Increment),
+        text(counter.value),
+        button("-").on_press(Message::Decrement),
+    ]
+        .into()
+}
+
+fn main() -> iced::Result {
+    run("Fuck given", update, view)
 }
